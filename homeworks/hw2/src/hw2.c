d2df8de35a51c0f2e4531663d84a60eed90d4d4e
Justin Huang <huangjk2@uci.edu>
2023-01-21 16:23:14 +0000

    hw2 initial commit

---
+++ homeworks/hw2/src/hw2.c
@@ -0,0 +1,73 @@
+// Your Name
+// NetId
+
+#include "hw2.h"
+#include "helpers2.h"
+#include "linkedlist.h"
+
+// Part 0 Function to implement
+char* myStrCpy(char* str, char* delimiters) {
+    return NULL;
+}
+
+// Part 1 Functions to implement
+ModFile* PutModFile(int ins, int dels, char* filename, ModFile* mf) {
+    return NULL;
+}
+
+int ModFileABC_Comparator(const void* file1, const void* file2) {
+    return -999;
+}
+
+int ModFileTotal_Comparator(const void* file1, const void* file2) {
+    return -999;
+}
+
+// Part 2 Functions to implement
+void ModFile_Printer(void* data, void* fp, int flag) {
+
+}
+
+void ModFile_Deleter(void* data) {
+
+}
+
+node_t* FindInList(list_t* list, void* token)  {
+    return NULL;
+}
+
+void DestroyList(list_t** list) {
+
+}
+
+void ProcessModFile(FILE* fp, list_t* list, char ordering) {
+
+}
+
+
+// Part 3 Functions to implement
+void AuthorPrinter(void* data, void *fp, int flag) {
+
+}
+
+int AuthorEmailComparator(const void* lhs, const void* rhs)  {
+    return -999;
+}
+
+int AuthorCommitComparator(const void* lhs, const void* rhs) {
+    return -999;
+}
+
+void AuthorDeleter(void* data)  {
+
+}
+
+Author* CreateAuthor(char* line, long int *timestamp)  {
+    return NULL;
+}
+
+
+// Part 4 Functions to implement
+void PrintNLinkedList(list_t* list, FILE* fp, int NUM) {
+
+}
